ad33f0b4764141a104344880dd63fb962f605068
CalumR23 <searagan@gmail.com>
2022-03-02 17:29:03 -0800

    POSHI-253 Import all utils used

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/VarPoshiElement.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/VarPoshiElement.java
@@ -33,6 +33,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 import org.dom4j.Attribute;
+import org.dom4j.CDATA;
 import org.dom4j.Element;
 import org.dom4j.Node;
 
