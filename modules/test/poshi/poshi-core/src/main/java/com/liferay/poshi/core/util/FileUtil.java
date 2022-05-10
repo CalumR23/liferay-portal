d9c001fd27e7dcb064776358200f7c4bef190e67
CalumR23 <searagan@gmail.com>
2022-05-10 14:35:53 -0700

    POSHI-290 SF

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/util/FileUtil.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/util/FileUtil.java
@@ -108,17 +108,6 @@ public class FileUtil {
 		return filePath;
 	}
 
-	public static void getFileContents(String filePath){
-		File directory = new File(filePath);
-
-		File[] filesList = directory.listFiles();
-		for(File f : filesList){
-			if(f.isDirectory() || f.isFile()) {
-				System.out.println(f.getName());
-			}
-		}
-	}
-
 	public static String getCanonicalPath(String filePath) {
 		try {
 			File file = new File(filePath);
