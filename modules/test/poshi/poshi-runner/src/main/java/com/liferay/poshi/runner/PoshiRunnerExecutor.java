a0e62ad4bafad49fd34bc80604d263d744c3f23a
CalumR23 <searagan@gmail.com>
2023-05-04 12:25:55 -0700

    more runner changes

--- modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerExecutor.java
+++ modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerExecutor.java
@@ -568,7 +568,7 @@ public class PoshiRunnerExecutor {
 
 					locator = PoshiContext.getPathLocator(
 						pathClassName + "#" + locatorKey,
-						PoshiGetterUtil.
+						_poshiStackTrace.
 							getNamespaceFromNamespacedClassCommandName(
 								locator));
 
