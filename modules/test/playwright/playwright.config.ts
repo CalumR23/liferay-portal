894011b42920f2aa813dfb0e91ba6e625398ee7d
CalumR23 <searagan@gmail.com>
2024-01-17 14:40:31 -0800

    TEMP link to right file

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from '@playwright/test';
 
 import {config as exportImportWeb} from './tests/export-import-web/config';
 import {config as batchPlanner} from './tests/batch-planner/config';
-import {config as analyticsCloud} from './tests/analytics-cloud/config';
+import {config as analyticsCloud} from './tests/analytics-settings-web/config';
 import {config as setup} from './tests/global.setup.config';
 import {config as object} from './tests/object-web/config';
 import { config as faro } from './tests/osb-faro-web/config';
