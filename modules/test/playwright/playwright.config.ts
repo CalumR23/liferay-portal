ca947b6c9ad26882737df7f8b641f760520068e4
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-01-23 14:22:15 -0300

    LRAC-0 Configure osb-faro-web and analytics-settings-web modules

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -6,7 +6,7 @@
 import {defineConfig} from '@playwright/test';
 
 import {config as batchPlannerConfig} from './tests/batch-planner/config';
-import {config as analyticsSettings} from './tests/analytics-settings-web/config';
+import {config as analyticsSettingsWeb} from './tests/analytics-settings-web/config';
 import {config as analyticsCloud} from './tests/analytics-cloud/config';
 import {config as clientExtensionWebConfig} from './tests/client-extension-web/config';
 import {config as commerceConfig} from './tests/commerce/config';
@@ -15,6 +15,7 @@ import {config as frontendDataSetViewsWebConfig} from './tests/frontend-data-set
 import {config as headlessBuilderWebConfig} from './tests/headless-builder-web/config';
 import {config as layoutContentPageEditorWebConfig} from './tests/layout-content-page-editor-web/config';
 import {config as objectWebConfig} from './tests/object-web/config';
+import {config as osbFaroWeb} from './tests/osb-faro-web/config';
 import {config as portalWebConfig} from './tests/portal-web/config';
 import {config as usersAdminWebConfig} from './tests/users-admin-web/config';
 
@@ -25,7 +26,7 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	projects: [
 		analyticsCloud,
-		analyticsSettings,
+		analyticsSettingsWeb,
 		batchPlannerConfig,
 		clientExtensionWebConfig,
 		commerceConfig,
@@ -34,6 +35,7 @@ export default defineConfig({
 		headlessBuilderWebConfig,
 		layoutContentPageEditorWebConfig,
 		objectWebConfig,
+		osbFaroWeb,
 		portalWebConfig,
 		usersAdminWebConfig,
 	],
