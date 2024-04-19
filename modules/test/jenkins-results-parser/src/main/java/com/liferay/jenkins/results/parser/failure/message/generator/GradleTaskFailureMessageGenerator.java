5c71372cb72225c12ccea492470a6520c824cabe
CalumR23 <searagan@gmail.com>
2024-04-19 09:50:03 -0700

    LRCI-4134 Add new method to grab gradle failure console text error

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/GradleTaskFailureMessageGenerator.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/GradleTaskFailureMessageGenerator.java
@@ -5,6 +5,8 @@
 
 package com.liferay.jenkins.results.parser.failure.message.generator;
 
+import com.liferay.jenkins.results.parser.Dom4JUtil;
+
 import org.dom4j.Element;
 
 /**
@@ -14,7 +16,7 @@ public class GradleTaskFailureMessageGenerator
 	extends BaseFailureMessageGenerator {
 
 	@Override
-	public Element getMessageElement(String consoleText) {
+	public String getMessage(String consoleText) {
 		if (!consoleText.contains(_TOKEN_WHAT_WENT_WRONG)) {
 			return null;
 		}
@@ -29,7 +31,12 @@ public class GradleTaskFailureMessageGenerator
 
 		start = consoleText.lastIndexOf("\n", start);
 
-		return getConsoleTextSnippetElementByStart(consoleText, start);
+		return getConsoleTextSnippetByStart(consoleText, start);
+	}
+
+	@Override
+	public Element getMessageElement(String consoleText) {
+		return Dom4JUtil.toCodeSnippetElement(getMessage(consoleText));
 	}
 
 	private static final String _TOKEN_WHAT_WENT_WRONG = "* What went wrong:";
