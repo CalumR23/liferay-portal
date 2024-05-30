9b8476b1385bcf79df7b3f1bdc0ebc2db43a382f
CalumR23 <searagan@gmail.com>
2024-05-30 15:18:31 -0700

    more outs

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/PlaywrightBatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/PlaywrightBatchTestClassGroup.java
@@ -168,6 +168,8 @@ public class PlaywrightBatchTestClassGroup extends BatchTestClassGroup {
 			if (segmentTestClassGroup instanceof
 					PlaywrightSegmentTestClassGroup) {
 
+				System.out.println("instance of playwright segment");
+
 				PlaywrightSegmentTestClassGroup
 					playwrightSegmentTestClassGroup =
 						(PlaywrightSegmentTestClassGroup)segmentTestClassGroup;
