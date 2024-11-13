7c5b38b45b0fae21944f510e6cf0a536728f7bf6
CalumR23 <searagan@gmail.com>
2024-11-13 09:59:14 -0800

    temp change set-up

--- modules/test/playwright/tests/stable/env/set_up.sh
+++ modules/test/playwright/tests/stable/env/set_up.sh
@@ -8,6 +8,24 @@ echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
 
 source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
-default_set_up
+update_portal_ext_properties
 
 prepare_additional_bundles
+
+	start_app_server
+
+	deploy_parent_project_osgi_modules
+
+	deploy_project_osgi_modules
+
+	deploy_parent_project_deploy_folder
+
+	deploy_project_deploy_folder
+
+	deploy_parent_project_osgi_configs
+
+	deploy_project_osgi_configs
+
+	deploy_parent_project_client_extensions
+
+	deploy_project_client_extensions
