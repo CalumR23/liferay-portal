9ad5a963e72fec30a2b87fc03f3e1606fb37925a
CalumR23 <searagan@gmail.com>
2025-10-09 10:29:44 -0700

    temp add skipped test

--- modules/test/playwright/tests/iframe-web/main/iframeWidget.spec.ts
+++ modules/test/playwright/tests/iframe-web/main/iframeWidget.spec.ts
@@ -19,6 +19,68 @@ const test = mergeTests(
 	pageViewModePagesTest
 );
 
+test.skip(
+	'Skip fake test',
+	{
+		tag: '@LPS-171989',
+	},
+	async ({apiHelpers, context, page, site, widgetPagePage}) => {
+
+		// Add listener with expect so it fails when a browser dialog is shown
+
+		page.on('dialog', async (dialog) => {
+			await dialog.accept();
+
+			expect(
+				dialog.message(),
+				'This alert should not be shown'
+			).toBeNull();
+		});
+
+		// Add widget page and navigate to view
+
+		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+			groupId: site.id,
+			title: getRandomString(),
+		});
+
+		await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
+
+		// Add iframe widget and configure it to show the home page of guest site
+
+		await widgetPagePage.addPortlet('IFrame');
+
+		await widgetPagePage.clickOnAction('IFrame', 'Configuration');
+
+		const configurationIFrame = page.frameLocator(
+			'iframe[title*="IFrame"]'
+		);
+
+		await configurationIFrame.getByLabel('Source URL').waitFor();
+
+		await configurationIFrame.getByLabel('Source URL').fill('/');
+
+		await configurationIFrame.getByRole('button', {name: 'Save'}).click();
+
+		await waitForAlert(
+			configurationIFrame,
+			'Success:You have successfully updated the setup.'
+		);
+
+		// Navigate to a new page and assert alert is not shown
+
+		const newPage = await context.newPage();
+
+		await newPage.goto(
+			`/web${site.friendlyUrlPath}${layout.friendlyURL}#constructor[prototype][comboBase]=data%3A%2Calert%281%29`
+		);
+
+		const viewIFrame = newPage.frameLocator('iframe[name*="IFrame"]');
+
+		await expect(viewIFrame.getByText('Welcome to Liferay')).toBeVisible();
+	}
+)
+
 test(
 	'Verifying js prototype pollution vulnerability in IFrame widget',
 	{
