c61409162058cf65fae344bb1c508d30d42035bc
CalumR23 <searagan@gmail.com>
2025-11-04 11:23:11 -0800

    temp

--- modules/sdk/gradle-plugins-ci-test-runner/src/main/java/com/liferay/gradle/plugins/ci/test/runner/CITestRunnerPlugin.java
+++ modules/sdk/gradle-plugins-ci-test-runner/src/main/java/com/liferay/gradle/plugins/ci/test/runner/CITestRunnerPlugin.java
@@ -68,12 +68,12 @@ public class CITestRunnerPlugin implements Plugin<Project> {
 						"https://archive.apache.org/dist/tomcat/tomcat-10" +
 							"/v10.1.42/bin/apache-tomcat-10.1.42.zip";
 
-					Project project = new Project();
-        			project.init();
+					org.apache.tools.ant.Project antProject = new org.apache.tools.ant.Project();
+        			antProject.init();
 
 					MirrorsGetTask mirrorsGetTask = new MirrorsGetTask();
 
-					mirrorsGetTask.setProject(project);
+					mirrorsGetTask.setProject(antProject);
 
 					mirrorsGetTask.setVerbose(true);
 					mirrorsGetTask.setSrc(url);
