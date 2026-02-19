c4d721200a9163e32a349c1f65e4ca28aaf0ed79
CalumR23 <searagan@gmail.com>
2026-02-19 09:51:21 -0800

    temp

--- modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/metrics/test-suite-report/js/main.js
+++ modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/metrics/test-suite-report/js/main.js
@@ -6,20 +6,16 @@ window.onload = function () {
 
 addReportName();
 
-if ((typeof categoryTableData !== 'undefined') && categoryTableData) {
-	let categoryTableDataElement = createTable(categoryTableData, 'test-suite-data-table');
-
-	window.onload = function () {
-		triggerEvent(getElementByXpath('//th[contains(.,"Test Suite Name")]'), 'click');
-
-		createBarChartFromTable('Daily Server Duration by Test Suite', 'server-duration-canvas', 'Total Server Duration', categoryTableDataElement, 'hrs');
-	}
-}
-
 if ((typeof tableData !== 'undefined') && tableData) {
 	let tableElement = createTable(tableData, 'test-suite-data-table');
 
 	addTotalColumn(tableElement);
 
-	Sortable.init();
+	window.onload = function () {
+		triggerEvent(getElementByXpath('//th[contains(.,"Test Suite Name")]'), 'click');
+
+		createBarChartFromTable('Daily Server Duration by Test Suite', 'server-duration-canvas', 'Total Server Duration', tableElement, 'hrs');
+	}
 }
+
+Sortable.init();
