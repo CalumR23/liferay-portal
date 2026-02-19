ee7acdb068943ae4699e2e5d432e6470a2ccde5e
CalumR23 <searagan@gmail.com>
2026-02-18 16:13:28 -0800

    LRCI-6878 Call bar chart function on page load

--- modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/metrics/test-suite-report/js/main.js
+++ modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/metrics/test-suite-report/js/main.js
@@ -6,6 +6,20 @@ window.onload = function () {
 
 addReportName();
 
+if ((typeof categoryTableData !== 'undefined') && categoryTableData) {
+	let categoryTableDataElement = createTable(categoryTableData, 'test-suite-data-table');
+
+	addUtilizationRows(categoryTableDataElement);
+
+	updateHeaderNames(categoryTableDataElement);
+
+	window.onload = function () {
+		triggerEvent(getElementByXpath('//th[contains(.,"Category")]'), 'click');
+
+		createBarChartFromTable('Daily Server Duration by Test Suite', 'server-duration-canvas', 'Total Server Duration', categoryTableDataElement);
+	}
+}
+
 if ((typeof tableData !== 'undefined') && tableData) {
 	let tableElement = createTable(tableData, 'test-suite-data-table');
 
