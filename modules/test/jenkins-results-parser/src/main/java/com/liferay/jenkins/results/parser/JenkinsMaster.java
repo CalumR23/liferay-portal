f102a21116cb6e6310dcd78c0fb6ce1cd1b93000
Peter Yoo <peter.yoo@liferay.com>
2026-08-07 15:52:50 -0700

    LRCI-7904 Rename
    isJenkinsSlaveOffline and _isJenkinsSlaveOffline differed by one
    underscore and meant different things: the public one excluded a node
    an operator had drained deliberately, the private one did not. Naming
    the public one for what it actually answers removes the trap.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsMaster.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsMaster.java
@@ -1291,7 +1291,8 @@ public class JenkinsMaster implements JenkinsNode<JenkinsMaster> {
 		}
 
 		public long getEstimatedDuration() {
-			return _currentExecutableJSONObject.optLong("estimatedDuration", -1);
+			return _currentExecutableJSONObject.optLong(
+				"estimatedDuration", -1);
 		}
 
 		public String getFullDisplayName() {
@@ -1336,7 +1337,7 @@ public class JenkinsMaster implements JenkinsNode<JenkinsMaster> {
 			return offlineCauseReason.contains("is being removed");
 		}
 
-		public boolean isJenkinsSlaveOffline() {
+		public boolean isJenkinsSlaveOfflineUnexpectedly() {
 			if (_isJenkinsSlaveOffline() &&
 				!_isJenkinsSlaveTemporarilyOffline()) {
 
